65b8c5b72c6f9bd2ad6a456bc515523a13b761eb
Daan Meijers <daan.meijers@yoursurprise.com>
2025-01-06 09:28:20 +0100

    updated iterator

--- lib/multimc-filesync/src/instance.rs
+++ lib/multimc-filesync/src/instance.rs
@@ -1,4 +1,4 @@
-use std::{fs::read_dir, path::PathBuf};
+use std::{fs::{read_dir, DirEntry}, path::PathBuf};
 
 use crate::error::{Error, ErrorKind};
 
@@ -41,45 +41,51 @@ fn index_instances(mmc_path: &PathBuf) -> Result<Vec<MMCInstance>, Error> {
         return Err(Error::new(ErrorKind::MultiMcFs, format!("{instance_dir_str} is not a directory.")));
     }
     
-    read_dir(instance_dir_path)
-        .map_err(|_| Error::new(ErrorKind::MultiMcFs, "Failed to list instances"))?
-        // TODO actually handle this exception
-        .filter(|entry_result| entry_result.is_ok())
-        .map(|entry_result| entry_result.unwrap())
+    let instances = read_dir(instance_dir_path)
+        .map_err(|err| Error::new(ErrorKind::MultiMcFs, format!("Failed to list instance dir: {}", err)))?
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|err| Error::new(ErrorKind::MultiMcFs, format!("Failed to list instance dir: {}", err)))?
+        .iter()
+        // TODO fix unwrapping
         .filter(|entry| entry.path().is_dir() && !entry.file_name().to_str().unwrap().starts_with("_"))
         .map(|entry| {
-            // TODO fix unwrapping
             let name = entry.file_name().into_string().unwrap();
             let path = entry.path();
             let saves = index_instance_saves(&path).unwrap();
-            Ok(MMCInstance {
+            MMCInstance {
                 name,
                 path: path.into_os_string().into_string().unwrap(),
                 saves
-            })
+            }
         })
-        .collect()
+        .collect();
+
+    Ok(instances)
 }
 
 fn index_instance_saves(instance_path: &PathBuf) -> Result<Vec<MMCSave>, Error> {
-    // Fetch instance name (directory name)
     let save_dir_path = instance_path.join(".minecraft").join("saves");
     if !save_dir_path.is_dir() {
         let instance_dir_str = save_dir_path.display();
         return Err(Error::new(ErrorKind::MultiMcFs, format!("{instance_dir_str} is not a directory.")));
     }
     
-    read_dir(save_dir_path)
-        .map_err(|_| Error::new(ErrorKind::MultiMcFs, "Failed to list saves"))?
-        .filter(|entry_result| entry_result.is_ok())
+    let saves = read_dir(save_dir_path)
+        .map_err(|err| Error::new(ErrorKind::MultiMcFs, format!("Failed to list saves dir: {}", err)))?
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|err| Error::new(ErrorKind::MultiMcFs, format!("Failed to list saves dir: {}", err)))?
+        .iter()
+        // TODO fix unwrapping
         .map(|entry_result| entry_result.unwrap())
         .map(|entry| {
             let name = entry.file_name().into_string().unwrap();
             let path = entry.path().into_os_string().into_string().unwrap();
-            Ok(MMCSave{
+            MMCSave{
                 name,
                 path
-            })
+            }
         })
-        .collect()
+        .collect();
+    
+    Ok(saves)
 }
